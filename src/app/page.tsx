2963d18d988258717c441317ee2c5184d343c279
Shaziasama <shaziasamma921@gmail.com>
2025-01-10 18:52:02 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+import Navbar from "../components/Navbar";
+import Mega from "../components/Mega";
+import AuthorCard from "../components/AuthorCard";
+import Featurre from "../components/Feature";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+<div>
+  
+  <Navbar />
+  <Featurre />
+  <Mega />
+  <AuthorCard />
+  <Footer />
+  
+</div>
+);
+}
